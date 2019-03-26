21dabbb7c0bca38ad63c8529d0843dbe12f110fd
Tamri, Hossam (IntlAssoc) <hossam.tamri@nist.gov>
2019-03-26 11:25:32 -0400

    fix missing attributes

--- src/app/modules/core/components/user-management-header/user-management-header.component.ts
+++ src/app/modules/core/components/user-management-header/user-management-header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManagementHeaderComponent implements OnInit {
 
+  loggedIn: boolean;
+  username: boolean;
+
   constructor() { }
 
   ngOnInit() {
